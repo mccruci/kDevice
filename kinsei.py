f35da7eac32a7946f551fe0be4cbd71e3fdbb569
mccruci <mc.cruciani@gmail.com>
2018-03-14 16:37:55 +0100

    modulo sqlalchemy v0.0

--- kinsei.py
+++ kinsei.py
@@ -1,8 +1,39 @@
 from .kinsei import *
 
 
+class CoordPersona(object):
+    """
+    modelle per le coordinate
+    """
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+class Device(CoordPersona):
+    def __init__(self,ip,listaCoord):
+        pass
+
+class Area(object):
+    ipDevice = ''
+    lCoodPer = ''
+
+    def setLCoodPer(self,listaOfDict):
+        """
+        :param listaOfDict: [{x:'',y:''}]
+        :return:
+        """
+        self.numero = len(listaOfDict)
+        pass
+
+
+
 
 class Kinsei(object):
     """
     questa classe si occupa di eseguire la connessione con i device e aggiornare il db
+    inoltra i dati della posizione
+    coordinatePersona-->Device
+    apro una connessione verso molti client con un dato ip, mia arrivano da ogni singolo ip le coordinata X,Y di ogni singola persona
+
     """
+
+    pass
